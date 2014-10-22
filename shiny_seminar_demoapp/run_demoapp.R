4b26ce5b494960e5dcfb254d68aaf9a6fa516ef7
bowerth <bo.werth@gmail.com>
2014-10-22 18:55:16 +0200

    add code examples

--- shiny_seminar_demoapp/run_demoapp.R
+++ shiny_seminar_demoapp/run_demoapp.R
@@ -1,17 +1,21 @@
 demopath <- file.path(dbpath, "GitHub", "as_seminar_shiny", "shiny_seminar_demoapp")
 
+shiny::runApp(file.path(demopath, "054-nvd3-line-chart-output"))
+
 # rChartsShiny
 shiny::runApp(file.path(demopath, "ramnathv_rChartsShiny"))
 
 # bins
-shiny::runApp(file.path(demopath, "default_bins"))
-shiny::runApp(file.path(demopath, "default_bins"), display.mode='showcase')
+shiny::runApp(file.path(demopath, "histogram"))
+shiny::runApp(file.path(demopath, "histogram"), display.mode='showcase')
 
 # k-means
 shiny::runApp(file.path(demopath, "jcheng_k-means"))
 shiny::runApp(file.path(demopath, "jcheng_k-means"), display.mode='showcase')
 
 shiny::runApp(file.path(demopath, "081-widgets-gallery"))
+
+
 
 # desk: industry
 shiny::runApp(file.path(dbpath, "GitHub", "desk", "inst", "industry"))
